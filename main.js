9c4c8c2133235b4838df4a39f0d7882d1086cf61
David Lau <David.lau@codeimmersives.com>
2020-04-08 14:58:53 -0400

    done

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
 const Ray = () => {
+  return {
+    length : 0,
+
+    push: function(val){
+      this[this.length] = val
+      this.length ++
+    },
+    pop: function(){
+      const val = this[this.length -1]
+      delete this[this.length -1]
+      this.length --
+      return val
+    },
+    unshift: function(val){
+    let next = this[0]
+    for (let i = 1 ; i < this.length+1; i++){
+      const old = next
+      next = this[i]
+      this[i] = old
+    }
+    this[0] = val
+    this.length ++
+     return this
+    },
+    shift: function(){
+      const val = this[0]
+      for (let i = 0 ; i < this.length ; i++){
+        this[i] = this[i+1]
+      }
+      delete this[this.length-1]
+      this.length -- 
+      return val
+    }
+
   
+  }
 }
+
 
 module.exports = {
   Ray,
 }
+
